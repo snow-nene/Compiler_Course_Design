35c87e8c0e70b4005a7152b69750fe7a167fc50a
XX <1453631832@qq.com>
2022-12-22 10:38:37 +0000

    lv1.2

---
+++ src/ast.h
@@ -0,0 +1,39 @@
+#pragma once
+// 所有 AST 的基类
+#include<iostream>
+#include <memory>
+class BaseAST {
+ public:
+  virtual ~BaseAST() = default;
+
+  virtual void Dump() const = 0;
+};
+
+
+class CompUnitAST : public BaseAST {
+ public:
+  std::unique_ptr<BaseAST> func_def;
+
+  void Dump() const override {
+    std::cout << "CompUnitAST { ";
+    func_def->Dump();
+    std::cout << " }";
+  }
+};
+
+class FuncDefAST : public BaseAST {
+ public:
+  std::unique_ptr<BaseAST> func_type;
+  std::string ident;
+  std::unique_ptr<BaseAST> block;
+
+  void Dump() const override {
+    std::cout << "FuncDefAST { ";
+    func_type->Dump();
+    std::cout << ", " << ident << ", ";
+    block->Dump();
+    std::cout << " }";
+  }
+};
+
+// ...
